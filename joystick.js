d481e567ac54da478cb50b53f146ddf60adab6cf
Leandro L Silva <leandro.lls@gmail.com>
2022-05-14 15:48:35 -0300

    random lane count and start lane

---
+++ joystick.js
@@ -0,0 +1,7 @@
+class Joystick {
+    constructor(canvas){
+      this.canvas=canvas;
+      this.x=canvas.right+100;
+      this.y=canvas.height*0.5;
+    }
+}
--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 const canvas=document.getElementById("sdc-canvas");
 canvas.height = window.innerHeight;
-canvas.width = 250;
+//canvas.width = 250;
 
 const ctx = canvas.getContext("2d");
+const maxLanes=4;
+const numLanes =  Math.floor(2+Math.random() * (maxLanes - 2));
+const carWidth=30;
+const carHeight=50;
+canvas.width=numLanes*(carWidth+20)/0.9;
+const canvasCenterX=canvas.width/2;
+const road = new Road(canvasCenterX, canvas.width*0.9,numLanes);
 
-const numLanes =  Math.floor(Math.random() * 4 + 1);
-const road = new Road(canvas.width/2, canvas.width*0.9,numLanes);
-
-const startLane =  Math.floor(Math.random() * numLanes);
+const startLane =  Math.floor(Math.random() * (numLanes));
 const carX=road.getLaneX(startLane);
-console.log([numLanes, startLane]);
-const carY=canvas.height*0.7;
+//alert([numLanes, startLane, canvas.width, canvasCenterX]);
+const fixedCameraPercent=0.7;
+const carY=canvas.height*fixedCameraPercent;
 
-const car = new Car(carX,carY,30,50);
+const car = new Car(carX,carY,carWidth,carHeight);
 road.draw(ctx);
 car.draw(ctx);
 
@@ -23,8 +28,13 @@ function animate(){
     car.update();
 
     canvas.height = window.innerHeight;
+    ctx.save();
+    ctx.translate(0,-car.y+canvas.innerHeight*fixedCameraPercent);
+    
     road.draw(ctx);
     car.draw(ctx);
+    
+    ctx.restore();
     requestAnimationFrame(animate);
 }
 
